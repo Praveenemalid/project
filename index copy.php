61c77a618e725194b93130a4d80ca1b839b9ca59
Praveen <praveen@trickuweb.com>
2021-01-25 02:52:45 +0530

    new

---
+++ index copy.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn CSS in one Video</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+<button class="mycolour">MY BUTTON</button>
+
+<!-- //Orderd List  -->
+<!-- //Unordered List -->
+
+<div class="element"></div>
+
+<ol>
+    <li>Mango</li>
+    <li>Banana</li>
+    <li>Orange</li>
+</ol>
+
+<ul>
+    <li>Mango</li>
+    <li>Banana</li>
+    <li>Orange</li>
+</ul>
+
+</body>
+</html>
